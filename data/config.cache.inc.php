55abdcb2d699a183d74ff2c01ff14942680e1fed
Lnta-Li <1107127328@qq.com>
2026-03-25 22:51:16 +0800

    2026

--- data/config.cache.inc.php
+++ data/config.cache.inc.php
@@ -180,9 +180,9 @@ $cfg_font_icon = '/* 在线链接服务仅供平台体验和调试使用，平�
 @font-face {
   font-family: \'iconfont\';  /* Project id 4858194 */
   src: 
-       url(\'//at.alicdn.com/t/c/font_4858194_6ja4odnbj43.woff2?t=1749798364689\') format(\'woff2\'),
-       url(\'//at.alicdn.com/t/c/font_4858194_6ja4odnbj43.woff?t=1749798364689\') format(\'woff\'),
-       url(\'//at.alicdn.com/t/c/font_4858194_6ja4odnbj43.ttf?t=1749798364689\') format(\'truetype\');
+       url(\'//at.alicdn.com/t/c/font_4858194_8vc0ijxppv3.woff2?t=1763974403013\') format(\'woff2\'),
+       url(\'//at.alicdn.com/t/c/font_4858194_8vc0ijxppv3.woff?t=1763974403013\') format(\'woff\'),
+       url(\'//at.alicdn.com/t/c/font_4858194_8vc0ijxppv3.ttf?t=1763974403013\') format(\'truetype\');
 }
 .iconfont {
     font-family:\'iconfont\' !important;
--- data/login.data.php
+++ data/login.data.php
@@ -1,2 +1,2 @@
 <?php
-$str_login='{"admin":"0,1742189573","Lntadmin":"0,1758013405","lntadmin":"0,1749831571"}';
+$str_login='{"admin":"0,1742189573","Lntadmin":"0,1774450070","lntadmin":"0,1749831571"}';
